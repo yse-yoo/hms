3ffee9b2e5ce57c047f3869890726d3d63c4fc07
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-07-10 15:58:04 +0900

    update

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ require_once 'app.php';
       <p class="text-gray-600">ラグジュアリーホテルが誇る贅沢な食事をお楽しみください。</p>
     </section>
 
-    <section>
+    <section class="p-6">
       <div id="menu" class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       </div>
     </section>
