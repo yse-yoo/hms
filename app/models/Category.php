336c59f52d3b7ab2b564a9d04cfe53d67e764511
yse-yoo <youhei_yoshikawa@yse-c.net>
2024-07-10 16:05:06 +0900

    update

--- app/models/Category.php
+++ app/models/Category.php
@@ -1,27 +1,41 @@
 <?php
-class Category
+class Category extends DB
 {
+    public $table_name = "categories";
 
-    public $values = [
-        ['id' => 1, 'name' => 'フード'],
-        ['id' => 2, 'name' => 'ドリンク'],
-    ];
+    // public $values = [
+    //     ['id' => 1, 'name' => 'フード'],
+    //     ['id' => 2, 'name' => 'ドリンク'],
+    // ];
 
     public function pluck()
     {
+        $this->get();
         return array_column($this->values, 'name', 'id');
     }
 
     public function findByID($id)
     {
-        $filtered = array_filter($this->values, function ($value) use ($id) {
-            return $value['id'] == $id;
-        });
-        return $filtered;
+        // $filtered = array_filter($this->values, function ($value) use ($id) {
+        //     return $value['id'] == $id;
+        // });
+        // return $filtered;
+
+        $posts['id'] = $id;
+        $sql = "SELECT * FROM {$this->table_name} WHERE id = :id;";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($posts);
+        $this->value = $stmt->fetch();
+        return $this->value;
     }
 
-    public function get()
+    public function get($limit = 20)
     {
+        // return $this->values;
+        $sql = "SELECT * FROM {$this->table_name} LIMIT {$limit}";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        $this->values = $stmt->fetchAll();
         return $this->values;
     }
 }
